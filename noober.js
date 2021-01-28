006b2cd66a1bba7fca222d15e53f547178d28819
Zach Hrabak <zach.hrabak@gmail.com>
2021-01-27 19:53:10 -0600

    Finished if else logic for ride conditions

--- noober.js
+++ noober.js
@@ -26,42 +26,72 @@ function pageLoad() {
   let numberOfRides = ride.length
   
   if (numberOfRides == 1){
-      if (ride[0].purpleRequested == true){
-      levelOfService = 'Noober Purple'
-      } else {
-      levelOfService = 'Noober X'
+      if (ride[0].numberOfPassengers > 3){
+        levelOfService = 'Noober XL'
+
+        passenger1Name = `${ride[0].passengerDetails.first} ${ride[0].passengerDetails.last}`
+        passenger1Phone = ride[0].passengerDetails.phoneNumber
+        passenger1NumberOfPassengers = ride[0].numberOfPassengers
+        passenger1PickupAddressLine1 = ride[0].pickupLocation.address
+        passenger1PickupAddressLine2 = `${ride[0].pickupLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}`
+        passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
+        passenger1DropoffAddressLine2 = `${ride[0].dropoffLocation.city}, ${ride[0].dropoffLocation.state} ${ride[0].dropoffLocation.zip}`
+      }
+      
+      else if (ride[0].purpleRequested == true){
+        levelOfService = 'Noober Purple'
+
+        passenger1Name = `${ride[0].passengerDetails.first} ${ride[0].passengerDetails.last}`
+        passenger1Phone = ride[0].passengerDetails.phoneNumber
+        passenger1NumberOfPassengers = ride[0].numberOfPassengers
+        passenger1PickupAddressLine1 = ride[0].pickupLocation.address
+        passenger1PickupAddressLine2 = `${ride[0].pickupLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}`
+        passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
+        passenger1DropoffAddressLine2 = `${ride[0].dropoffLocation.city}, ${ride[0].dropoffLocation.state} ${ride[0].dropoffLocation.zip}`
+      } 
+      
+      else {
+        levelOfService = 'Noober X'
+
+        passenger1Name = `${ride[0].passengerDetails.first} ${ride[0].passengerDetails.last}`
+        passenger1Phone = ride[0].passengerDetails.phoneNumber
+        passenger1NumberOfPassengers = ride[0].numberOfPassengers
+        passenger1PickupAddressLine1 = ride[0].pickupLocation.address
+        passenger1PickupAddressLine2 = `${ride[0].pickupLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}`
+        passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
+        passenger1DropoffAddressLine2 = `${ride[0].dropoffLocation.city}, ${ride[0].dropoffLocation.state} ${ride[0].dropoffLocation.zip}`
       }
   }
-   else if (numberOfRides 4){
+
+  else {
     levelOfService = 'Noober Pool'
-  } else {
-    levelOfServie = "Noober XL"
+
+    passenger1Name = `${ride[0].passengerDetails.first} ${ride[0].passengerDetails.last}`
+    passenger1Phone = ride[0].passengerDetails.phoneNumber
+    passenger1NumberOfPassengers = ride[0].numberOfPassengers
+    passenger1PickupAddressLine1 = ride[0].pickupLocation.address
+    passenger1PickupAddressLine2 = `${ride[0].pickupLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}`
+    passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
+    passenger1DropoffAddressLine2 = `${ride[0].dropoffLocation.city}, ${ride[0].dropoffLocation.state} ${ride[0].dropoffLocation.zip}`
+
+    passenger2Name = `${ride[1].passengerDetails.first} ${ride[1].passengerDetails.last}`
+    passenger2Phone = ride[1].passengerDetails.phoneNumber
+    passenger2NumberOfPassengers = ride[1].numberOfPassengers
+    passenger2PickupAddressLine1 = ride[1].pickupLocation.address
+    passenger2PickupAddressLine2 = `${ride[1].pickupLocation.city}, ${ride[1].pickupLocation.state} ${ride[1].pickupLocation.zip}`
+    passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
+    passenger2DropoffAddressLine2 = `${ride[1].dropoffLocation.city}, ${ride[1].dropoffLocation.state} ${ride[1].dropoffLocation.zip}`
+    
+    passenger3Name = `${ride[2].passengerDetails.first} ${ride[2].passengerDetails.last}`
+    passenger3Phone = ride[2].passengerDetails.phoneNumber
+    passenger3NumberOfPassengers = ride[2].numberOfPassengers
+    passenger3PickupAddressLine1 = ride[2].pickupLocation.address
+    passenger3PickupAddressLine2 = `${ride[2].pickupLocation.city}, ${ride[2].pickupLocation.state} ${ride[2].pickupLocation.zip}`
+    passenger3DropoffAddressLine1 = ride[2].dropoffLocation.address
+    passenger3DropoffAddressLine2 = `${ride[2].dropoffLocation.city}, ${ride[2].dropoffLocation.state} ${ride[2].dropoffLocation.zip}`
   }
+
   
-
-  passenger1Name = `${ride[0].passengerDetails.first} ${ride[0].passengerDetails.last}`
-  passenger1Phone = ride[0].passengerDetails.phoneNumber
-  passenger1NumberOfPassengers = ride[0].numberOfPassengers
-  passenger1PickupAddressLine1 = ride[0].pickupLocation.address
-  passenger1PickupAddressLine2 = `${ride[0].pickupLocation.city}, ${ride[0].pickupLocation.state} ${ride[0].pickupLocation.zip}`
-  passenger1DropoffAddressLine1 = ride[0].dropoffLocation.address
-  passenger1DropoffAddressLine2 = `${ride[0].dropoffLocation.city}, ${ride[0].dropoffLocation.state} ${ride[0].dropoffLocation.zip}`
-
-  passenger2Name = `${ride[1].passengerDetails.first} ${ride[1].passengerDetails.last}`
-  passenger2Phone = ride[1].passengerDetails.phoneNumber
-  passenger2NumberOfPassengers = ride[1].numberOfPassengers
-  passenger2PickupAddressLine1 = ride[1].pickupLocation.address
-  passenger2PickupAddressLine2 = `${ride[1].pickupLocation.city}, ${ride[1].pickupLocation.state} ${ride[1].pickupLocation.zip}`
-  passenger2DropoffAddressLine1 = ride[1].dropoffLocation.address
-  passenger2DropoffAddressLine2 = `${ride[1].dropoffLocation.city}, ${ride[1].dropoffLocation.state} ${ride[1].dropoffLocation.zip}`
-
-  passenger3Name = `${ride[2].passengerDetails.first} ${ride[2].passengerDetails.last}`
-  passenger3Phone = ride[2].passengerDetails.phoneNumber
-  passenger3NumberOfPassengers = ride[2].numberOfPassengers
-  passenger3PickupAddressLine1 = ride[2].pickupLocation.address
-  passenger3PickupAddressLine2 = `${ride[2].pickupLocation.city}, ${ride[2].pickupLocation.state} ${ride[2].pickupLocation.zip}`
-  passenger3DropoffAddressLine1 = ride[2].dropoffLocation.address
-  passenger3DropoffAddressLine2 = `${ride[2].dropoffLocation.city}, ${ride[2].dropoffLocation.state} ${ride[2].dropoffLocation.zip}`
 
   //  👆 YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE 👆
 }
